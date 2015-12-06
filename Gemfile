353a8bb44ad1cedabda6f918c12730aec9b60a00
Thomas Ritter <thomas.ritter@simplificator.com>
2015-12-06 22:39:07 +0100

    Add test for ProfileRepository

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '1.4.6'
 gem 'byebug'
+gem 'minitest'
---
+++ test/test_profile_repository.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require "./profile_repository"
+
+class TestProfileRepository < Minitest::Test
+
+  def test_that_all_profiles_are_loaded
+    assert_equal 3, ProfileRepository.all_profiles.size
+  end
+
+  def test_that_all_profiles_are_loaded
+    profile = ProfileRepository.all_profiles[0]
+
+    assert_equal 'Peter', profile.name
+    assert_equal 'peter', profile.nickname
+    assert_equal '10.06.1986', profile.date_of_birth
+    assert_equal 'peter.jpg', profile.image_file
+    assert_equal ['Swimming', 'travelling'], profile.hobbies
+    assert_equal ['ines', 'shirley'], profile.friends
+  end
+
+end
